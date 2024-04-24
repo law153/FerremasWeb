9b0f52c3cfe2115c042cc21e5d9890f017c46dd6
Abel <abelx3678@gmail.com>
2024-04-24 16:42:50 -0400

    urls agregado

---
+++ FerremasWeb/core/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import mostrarIndex, mostrarLogin, inicioSesion, cierreSesion, mostrarProductos, mostrarProducto, mostrarCarrito, agregarAlCarrito, cambiarCantidad, sacarDelCarro
+
+urlpatterns=[
+
+    ###No cuenta
+    path('',mostrarIndex,name="mostrarIndex"),
+    path('login/',mostrarLogin,name="mostrarLogin"),
+    path('productos/<id_cate>',mostrarProductos,name="mostrarProductos"),
+    path('producto/<id_prod>',mostrarProducto,name="mostrarProducto"),
+    path('carrito',mostrarCarrito,name="mostrarCarrito"),
+    path('inicioSesion/',inicioSesion, name="inicioSesion"),
+    path('cierreSesion/',cierreSesion,name="cierreSesion"),
+    path('cambiarCantidad/<cod_detalle>',cambiarCantidad,name="cambiarCantidad"),
+    path('sacarDelCarro/<cod_detalle>',sacarDelCarro,name="sacarDelCarro"),
+    path('agregarAlCarrito/',agregarAlCarrito,name="agregarAlCarrito"),
+]
